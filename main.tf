4b6817fe4c9d6dd2570ebfa476d16321271c8c41
Marianne Infantas <marianneinfantas@gmail.com>
2025-07-17 07:49:29 -0400

    Correción Terraform

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
